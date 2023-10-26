2c42202139e3ad924e6f1dede0d2275524dddd32
HerrBergenholtz <zbdv8800@gmail.com>
2023-10-27 00:01:21 +0200

    Add project files.

---
+++ Uppgift 4.9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Uppgift_4._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ut sidlängden för en triangel");
+            int sidLängd = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <sidLängd; i++)
+            {
+                Console.WriteLine("");
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
